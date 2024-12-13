95986070980bfbd947aafe1e409489626283b9b1
jiyoon <rlawldbs9128@gmail.com>
2024-12-14 02:26:24 +0900

    [update] 6. 필요한 부분만 갱신하기 -@DynamicInsert를 적용하여 DB의 기본값 적용

--- src/main/java/com/example/demo/entity/Item.java
+++ src/main/java/com/example/demo/entity/Item.java
@@ -2,28 +2,33 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.DynamicInsert;
 
 
 @Entity
 @Getter
+@DynamicInsert //DynamicInsert를 사용하여 null인 필드는 제외되고 DB의 기본값이 적용됨
 // TODO: 6. Dynamic Insert
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String description;
 
-    @ManyToOne
-    @JoinColumn(name = "owner_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
 
-    @ManyToOne
-    @JoinColumn(name = "manager_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id", nullable = false)
     private User manager;
 
+    //status가 null일 경우, INSERT SQL에서 제외되고, DB 기본값 PENDING이 적용
     @Column(nullable = false, columnDefinition = "varchar(20) default 'PENDING'")
     private String status;
 
